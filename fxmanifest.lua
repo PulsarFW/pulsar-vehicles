003df27cfa509d7645d92c5568c2ad8f4a7a8fec
Artmines <arturowells@gmail.com>
2026-04-28 15:26:45 -0230

    feat(init):Initial version

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version("cerulean")
+games({ "gta5" })
+lua54("yes")
+client_script("@pulsar-core/exports/cl_error.lua")
+client_script("@pulsar-pwnzor/client/check.lua")
+
+client_scripts({
+	'@pulsar-polyzone/client.lua',
+	'@pulsar-polyzone/BoxZone.lua',
+	'@pulsar-polyzone/EntityZone.lua',
+	"client/**/*.lua",
+})
+
+server_scripts({
+	'@oxmysql/lib/MySQL.lua',
+	"server/**/*.lua",
+})
+
+shared_scripts({
+	"@ox_lib/init.lua",
+	"shared/**/*.lua",
+})
